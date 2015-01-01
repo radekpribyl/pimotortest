e4519e458f04a041a375324921fa6df64ae754dd
Radek Pribyl <pribyl@atlas.cz>
2015-01-01 19:39:20 +0100

    Making web server externally visible

--- MotorTest/runwebserver.py
+++ MotorTest/runwebserver.py
@@ -5,10 +5,10 @@ from os import environ
 
 if __name__ == '__main__':
     app.config.from_object(DevelopmentConfig)
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    #HOST = environ.get('SERVER_HOST', 'localhost')
     #try:
     #    PORT = int(environ.get('SERVER_PORT', '5555'))
     #except ValueError:
     #    PORT = 5555
     PORT = 5555
-    app.run(HOST, PORT)
+    app.run(host='0.0.0.0', port=PORT)
