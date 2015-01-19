4cfd0598d29d09d235c025d96b4374e34563aac8
Radek Pribyl <pribyl@atlas.cz>
2015-01-19 20:07:14 +0100

    Modified malinaconfig

--- MotorTest/malina.py
+++ MotorTest/malina.py
@@ -2,10 +2,10 @@ from pi2golite import Pi2GoLiteConfig, Robot
 
 class MalinaConfig(Pi2GoLiteConfig):
      def __init__(self):
-         motor_left['revcorr'] = 7.6
-         wheelsensors['avail'] = True
-         servos['avail'] = True
-         servos['param']['maxsteps'] = 238
+         self.motor_left['revcorr'] = 7.6
+         self.wheelsensors['avail'] = True
+         self.servos['avail'] = True
+         self.servos['param']['maxsteps'] = 238
 
 r = Robot(MalinaConfig())
 r.init()
